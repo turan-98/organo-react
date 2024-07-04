a81a60a7fd108cbde62bc8eb2ede64d33cc5a464
turan-98 <santanasamuel08@gmail.com>
2024-07-04 12:39:45 -0300

    🎨 Melhorando estrutura do código

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import Banner from './components/Banner';
 import Form from './components/Form';
 import Time from './components/Time';
+import React from 'react';
 
 import { useState } from 'react';
 
 function App() {
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria:'#57c2f8',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Front-End',
+      corPrimaria:'#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Data Science',
+      corPrimaria:'#57c2f8',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'DevOps',
+      corPrimaria:'#82cffa',
+      corSecundaria: '#e8f8ff'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria:'#57c2f8',
+      corSecundaria: '#d9f7e9'
+    },
+    {
+      nome: 'Gestão',
+      corPrimaria:'#82cffa',
+      corSecundaria: '#e8f8ff'
+    }
+
+  ]
 
   const [coworkers, setCoworker] = useState([])
 
@@ -14,9 +48,9 @@ function App() {
   return (
     <div className="App">
         <Banner />
-        <Form onSaveCoworker={coworker => newCoWorker(coworkers)}/>
-        <Time nome="Programação"/>
-        <Time nome="Front-End"/>
+        <Form onSaveCoworker={coworker => newCoWorker(coworker)}/>
+        {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
+    
     </div>
   );
 }
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,12 +1,20 @@
 import './Time.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Time = (props) => {
-
+    const css = {backgroundColor: props.corSecundaria}
     return(
-        <section className='time'>
-            <h3>{props.nome}</h3>
+
+        <section className='time' style={css}>
+            <h3 style={{borderColor:props.corPrimaria}}>{props.nome}</h3>
         </section>
     )
 
+}
+Time.propTypes ={
+    nome: PropTypes.func.isRequired,
+    corPrimaria: PropTypes.func.isRequired,
+    corSecundaria: PropTypes.func.isRequired
 }
 export default Time;
